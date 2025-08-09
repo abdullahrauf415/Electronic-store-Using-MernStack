be1ef90cda58fd5a903e7f593b4e93b2a7448123
Abdullahrauf <abdullahrauf415@gmail.com>
2025-08-09 11:12:11 +0500

    Commit

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import HomeProvider from "./Context/HomeProvider.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HomeProvider>
       <App />
+      <SpeedInsights />
     </HomeProvider>
   </StrictMode>
 );
